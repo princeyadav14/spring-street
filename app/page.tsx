ed5b03b354d7a22bb688046e4586680441797fdd
Prince Yadav <princeyadav3368@gmail.com>
2026-06-01 11:37:43 +0530

    Initial project setup

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+ export default function Home() {
+  return (
+    <main className="bg-black text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Spring Street</h1>
+    </main>
+  )
+}
